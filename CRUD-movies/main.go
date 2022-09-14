025f3832a065043a84ac2fe803f76f8fcdaff48a
huzidev <huzaifa.iqdev@gmail.com>
2022-09-14 18:00:32 +0500

    CRUD operations

--- CRUD-movies/main.go
+++ CRUD-movies/main.go
@@ -1,14 +1,14 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"encoding/json"
-// 	"math/rand"
-// 	"net/http"
-// 	"strconv" // because id will INT and we've to make it String
-// 	"github.com/gorilla/mux"
-// )
+import (
+	// 	"fmt"
+	// 	"log"
+	// 	"encoding/json"
+	// 	"math/rand"
+	// 	"net/http"
+	// 	"strconv" // because id will INT and we've to make it String
+	"github.com/gorilla/mux"
+)
 
 type Movie struct {
 	ID       string    `json:"id"`
@@ -25,5 +25,12 @@ type Director struct {
 var movies []Movie
 
 func main() {
+	r := mux.NewRouter()
 
+	// functions
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("PUT")
+	r.HandleFunc("/movies{id}", updateMovie).Methods("POST")
+	r.HandleFunc("/movies{id}", deleteMovie).Methods("DELETE")
 }
