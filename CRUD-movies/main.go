71c1ec576c1874b63f0e30c7a6656bbf58045df3
huzidev <huzaifa.iqdev@gmail.com>
2022-09-14 17:55:48 +0500

    main fucntions

--- CRUD-movies/main.go
+++ CRUD-movies/main.go
@@ -14,10 +14,16 @@ type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
 	Title    string    `json:"title"`
-	Director *Director `json:"director"` // every movie has a director and all the types of
+	Director *Director `json:"director"` // every movie has a director and all the types of Director
 }
 
 type Director struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
 }
