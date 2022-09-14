d2c27e2fd0d87e6bbd75a9d53a27ba3e93ec1b15
huzidev <huzaifa.iqdev@gmail.com>
2022-09-14 17:18:30 +0500

    local host port

--- web-server/main.go
+++ web-server/main.go
@@ -41,7 +41,8 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/form", formHandler)
 
-	fmt.Printf("Starting server at port 8000\n")
+	port := 8000
+	fmt.Printf("Starting server at port %v\n", port)
 
 	// err != nil mean (err is not empty) hence some error occurred
 	if err := http.ListenAndServe(":8000", nil); err != nil {
