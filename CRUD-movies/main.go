909c6c35d3138c0852dba6f204cbb968282f515f
huzidev <huzaifa.iqdev@gmail.com>
2022-09-14 17:37:03 +0500

    struct movies

---
+++ CRUD-movies/main.go
@@ -0,0 +1,14 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"encoding/json"
+// 	"math/rand"
+// 	"net/http"
+// 	"strconv" // because id will INT and we've to make it String
+// 	"github.com/gorilla/mux"
+// )
+
+type Movie struct {
+}
